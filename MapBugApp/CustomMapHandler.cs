eed2586285752c44cea8b70a58ffcd36dbdeef6f
ADWA <adwa@demant.com>
2024-02-15 15:39:56 +0100

    crash reproduced

--- MapBugApp/CustomMapHandler.cs
+++ MapBugApp/CustomMapHandler.cs
@@ -4,5 +4,15 @@ namespace MapBugApp;
 
 public class CustomMapHandler : MapHandler
 {
+    public CustomMapHandler()
+    {
+        
+    }
     
+    #if IOS
+    protected override void DisconnectHandler(Microsoft.Maui.Maps.Platform.MauiMKMapView platformView)
+    {
+        base.DisconnectHandler(platformView);
+    }
+#endif
 }
--- MapBugApp/MapPage.xaml.cs
+++ MapBugApp/MapPage.xaml.cs
@@ -1,3 +1,5 @@
+using Microsoft.Maui.Maps;
+
 namespace MapBugApp;
 
 public partial class MapPage : ContentPage
@@ -5,10 +7,27 @@ public partial class MapPage : ContentPage
     public MapPage()
     {
         InitializeComponent();
+
+        MoveMap();
+        
+
+    }
+
+    private async void MoveMap()
+    {
+        await Task.Delay(1000).ConfigureAwait(false);
+        
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            var mapSpan = MapSpan.FromCenterAndRadius(new Location(5, 5), Distance.FromMeters(10000));
+
+            Map.MoveToRegion(mapSpan);
+        });
     }
 
     private void GoBack(object? sender, EventArgs e)
     {
+        Map.Handler?.DisconnectHandler();
         Navigation.PopAsync();
     }
 }
