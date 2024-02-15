dcafb7ebda64110bf8b3e2a1c4b2e6ce81bc2077
ADWA <adwa@demant.com>
2024-02-15 14:32:28 +0100

    repo init

---
+++ MapBugApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MapBugApp;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ MapBugApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MapBugApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
